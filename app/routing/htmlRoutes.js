1e8774a77151f01fe5d8bb8abd4468820eec3db3
SaulValo <SaulValo@MacBook-Pro-2.local>
2017-04-29 00:36:26 -0700

    added changes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,7 +6,6 @@ module.exports = function(newApp) {
     res.sendFile(path.join(__dirname + '/../public/survey.html'));
   })
   
-  // if using the app and url is not defined send to home page
   newApp.use(function(req, res) {
     res.sendFile(path.join(__dirname + '/../public/home.html'));
   })
